a4373608f6abcae85a931a7431562e25229e222a
Artemiusz Kuzniecow <arty.kuznetsov@yahoo.com>
2023-06-11 20:07:28 +0300

    feat: keep creating some config files for the project

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+
+module.exports = (env, argv) => {
+    const isProd = argv.mode === "production"
+    const isDev = !isProd
+
+    const filename = ext  => isProd ? `[name].[contenthash].bundle.${ext}`:`[name].bundle.${ext}`
+
+    return {
+            context: path.resolve(__dirname, "src"),
+            entry: {
+                main: [
+                    "core-js/stable",
+                    "regenerator-runtime/runtime",
+                    "./index.js"
+                    ]
+            },
+            output: {
+                path: path.resolve(__dirname, "dist"),
+                filename: filename("js"),
+                clean: true
+            },
+            resolve:{
+                extensions: [".js"],
+                alias: {
+                    "@": path.resolve(__dirname, "src"),
+                    "@core": path.resolve(__dirname, "core"),
+                },
+            },
+            devServer: {
+                port: 3000,
+                open: true,
+                hot: false,
+                watchFiles: './',
+            },
+            plugins: [
+                new HtmlWebpackPlugin({
+                    template: "./index.html"
+                }),
+                new CopyPlugin({
+                    patterns: [
+                      { from: path.resolve(__dirname, "src/favicon", "favicon.ico"), 
+                        to: path.resolve(__dirname, "dist") },
+                    ],
+                }),
+                new MiniCssExtractPlugin({
+                    filename: filename("css")
+                })
+            ],
+            devtool: isDev ? "source-map" : false,
+            module: {
+                rules:  [
+                            {
+                                test: /\.s[ac]ss$/i,
+                                use: [
+                                MiniCssExtractPlugin.loader,
+                                "css-loader",
+                                "sass-loader",
+                                ],
+                            },
+                            {
+                                test: /\.m?js$/,
+                                exclude: /node_modules/,
+                                use: {
+                                        loader: "babel-loader",
+                                        options: {
+                                        presets: ['@babel/preset-env']
+                                    }
+                        }
+                    }
+                ],
+            },
+    }
+}
