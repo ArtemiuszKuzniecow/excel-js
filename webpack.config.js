e19f3e917d4918f4ffdffe520cced320be90f9fd
Artemiusz Kuzniecow <arty.kuznetsov@yahoo.com>
2023-06-14 07:02:05 +0300

    feat: start adding some css for the project

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = (env, argv) => {
   const isDev = !isProd;
 
   const filename = (ext) =>
-        isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`;
+    isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`;
 
   const plugins = () => {
     const base = [
@@ -30,7 +30,7 @@ module.exports = (env, argv) => {
       }),
 
     ];
-    if (isDev) base.push(new ESLintPlugin(options));
+    if (isDev) base.push(new ESLintPlugin());
     return base;
   };
 
@@ -64,7 +64,7 @@ module.exports = (env, argv) => {
     plugins: plugins(),
     devtool: isDev ? 'source-map' : false,
     module: {
-      rules: [
+      'rules': [
         {
           test: /\.s[ac]ss$/i,
           use: [
