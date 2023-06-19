f9b6d7a6e5a11e15993da4c462c06e3e4e867d05
Artemiusz Kuzniecow <arty.kuznetsov@yahoo.com>
2023-06-19 06:44:05 +0300

    feat: keep creating a framework

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -3,8 +3,20 @@ import {ExcelComponent} from '@core/ExcelComponent';
 export class Formula extends ExcelComponent {
   static className = 'excel__formula';
 
+  constructor($root) {
+    super($root, {
+      name: 'Formula',
+      listeners: ['input'],
+    });
+  }
+
   toHTML() {
     return `<div class="excel__formula-info">fx</div>
             <div class="excel__formula-input" contenteditable spellcheck="false"></div>`;
+  }
+
+  onInput() {
+    console.log(this.$root);
+    console.log('Formumla: onInput', event);
   }
 }
