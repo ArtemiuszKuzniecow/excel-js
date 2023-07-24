f7f481468869c19913bfdfaf200d7999a537dd65
Artemiusz Kuzniecow <arty.kuznetsov@yahoo.com>
2023-07-24 05:54:52 +0300

    feat: added redux class

---
+++ src/core/createStore.js
@@ -0,0 +1,25 @@
+export class Store {
+  constructor(rootReducer, initialState = {}) {
+    this.rootReducer = rootReducer;
+    this.state = rootReducer({...initialState}, {type: '__INIT__'});
+    this.listeners = [];
+  }
+
+  subscribe(fn) {
+    this.listeners.push(fn);
+    return {
+      unsubsribe() {
+        this.listeners = this.listeners.filter((listener = listener !== fn));
+      },
+    };
+  }
+
+  dispatch(action) {
+    this.state = this.rootReducer(this.state, action);
+    this.listeners.forEach((listener) => listener());
+  }
+
+  getState() {
+    return this.state;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+import {Store} from '@/core/createStore';
 import {Excel} from '@//components/excel/Excel';
 import {Formula} from '@//components/formula/Formula';
 import {Table} from '@//components/table/Table';
 import {Toolbar} from '@//components/toolbar/Toolbar';
 import {Header} from '@/components/header/Header';
+import {rootReducer} from '@/redux/rootReducer';
 import './scss/index.scss';
 
-const excel = new Excel('#app', {components: [Header, Toolbar, Formula, Table]});
+const store = new Store(rootReducer);
+
+const excel = new Excel('#app', {components: [Header, Toolbar, Formula, Table], store});
 
 excel.render();
