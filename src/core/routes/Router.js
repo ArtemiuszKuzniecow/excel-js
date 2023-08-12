0b5219c99c258aadca63bd92cad687a3bb320329
Artemiusz Kuzniecow <arty.kuznetsov@yahoo.com>
2023-08-12 09:53:41 +0300

    feat: started creating router

---
+++ src/core/routes/Router.js
@@ -0,0 +1,31 @@
+import {$} from '@/core/dom';
+import {ActiveRoute} from './ActiveRoute';
+
+export class Router {
+  constructor(selector, routes) {
+    if (!selector) {
+      throw new Error('Selector is required');
+    }
+
+    this.$placeholder = $(selector);
+    this.routes = routes;
+
+    this.changePageHandler = this.changePageHandler.bind(this);
+
+    this.init();
+  }
+
+  init() {
+    window.addEventListener('hashchange', this.changePageHandler);
+    this.changePageHandler();
+  }
+
+  changePageHandler() {
+    console.log(ActiveRoute.path);
+    this.$placeholder.html(ActiveRoute.path);
+  }
+
+  destroy() {
+    window.removeEventListener('hashchange', this.changePageHandler);
+  }
+}
